64ec4691036cd75a1fbc5f24cd6240067bf1b214
razmatinyan <razmatinyan28@gmail.com>
2023-09-21 23:04:11 +0400

    init starting payments natours

---
+++ utils/email.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+class Email {
+	constructor(user, url) {
+		this.to = user.email;
+		this.firstName = user.name.split(' ')[0];
+		this.url = url;
+		this.from = `${user.name} <${process.env.EMAIL_FROM}>`;
+	}
+
+	newTransport() {
+		return nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD
+			}
+		});
+	}
+
+	async send(template, subject) {
+		const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+			firstName: this.firstName,
+			url: this.url,
+			subject
+		});
+
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html)
+		};
+
+		await this.newTransport().sendMail(mailOptions);
+	}
+
+	async sendWelcome() {
+		await this.send('Welcome', 'Welcome to the Natours!');
+	}
+
+	async sendPasswordReset() {
+		await this.send('passwordReset', 'Your password reset token (valid for only 10 minutes)');
+	}
+}
+
+module.exports = Email;
